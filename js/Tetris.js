34bc2604d721d7ca05cf7a59dcdce5eca1b4d965
pyjun01 <pyjun01@naver.com>
2018-12-25 01:30:13 +0900

    ??

--- js/Tetris.js
+++ js/Tetris.js
@@ -1,4 +1,5 @@
 (function (){
+	var T= 500;
 	function Tetris(){
 		this.canvas= document.querySelector("#canvas");
 		this.ctx= this.canvas.getContext('2d');
@@ -6,300 +7,219 @@
 		this.H= this.canvas.height;
 		this.Bw= this.W/10;
 		this.Bh= 40;
-
-
-		var self= this;
-
-		this.shape_name= ["I","O","T","L","J","S","Z"];
-		this.shape_draw={
-			I: function (x, y){
-				var c= "#364fc7";
-				self.block(x*self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw*2, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw*3, y*self.Bh+20, c);
-			},
-			O: function (x, y){
-				var c= "#fcc419";
-				self.block(x*self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw, y*self.Bh+20-self.Bw, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20-self.Bw, c);
-			},
-			T: function (x, y){
-				var c= "#845ef7";
-				self.block(x*self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw*2, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20-self.Bh, c);
-			},
-			L: function (x, y){
-				var c= "#e8590c";
-				self.block(x*self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw, y*self.Bh+20-self.Bh, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw*2, y*self.Bh+20, c);
-			},
-			J: function (x, y){
-				var c= "#74c0fc";
-				self.block(x*self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw*2, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw*2, y*self.Bh+20-self.Bh, c);
-			},
-			S: function (x, y){
-				var c= "#8ce99a";
-				self.block(x*self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20-self.Bh, c);
-				self.block(x*self.Bw+self.Bw*2, y*self.Bh+20-self.Bh, c);
-			},
-			Z: function (x, y){
-				var c= "#f03e3e";
-				self.block(x*self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20, c);
-				self.block(x*self.Bw+self.Bw, y*self.Bh+20+self.Bh, c);
-				self.block(x*self.Bw+self.Bw*2, y*self.Bh+20+self.Bh, c);
-			},
-		};
 	}
 	Tetris.prototype.sort = function() {
-		this.ShapeList= [];
 		var self= this;
+		var a= ["I","O","T","L","J","S","Z"];
+		var arr= [];
 		function recursion (){
-			var R= Math.floor(Math.random()*7);
-			var n= 0;
-			for(var i=0; i<self.ShapeList.length; i++){
-				if(self.ShapeList[i] != R)
-					n++;
+			var R= Math.floor(Math.random()*a.length);
+			arr.push(a[R]);
+			a.splice(R, 1);
+			if(a.length==0){
+				self.ShapeList= arr;
+				return;
 			}
-			if(n==self.ShapeList.length)
-				self.ShapeList.push(R);
-			if(self.ShapeList.length==7)
-				return false;
-			return recursion();
+			recursion();
 		}
 		recursion();
 	};
-	Tetris.prototype.init = function (){
-		function getX(){
-			var arr= [];
-			switch(self.shape_name[self.ShapeList[self.cnt]]){
+	Tetris.prototype.getPos = function(xy) {
+		var arr= [];
+		if(xy){
+			switch(this.ShapeList[this.cnt]){
 				case "I":
 					arr= [3, 4, 5, 6];
 					break;
 				case "O":
-					arr= [3, 4];
+					arr= [3, 4, 3, 4];
 					break;
 				case "T":
-					arr= [3, 4, 5];
+					arr= [4, 3, 4, 5];
 					break;
 				case "L":
-					arr= [3, 4, 5];
+					arr= [3, 3, 4, 5];
 					break;
 				case "J":
-					arr= [3, 4, 5];
+					arr= [5, 3, 4, 5];
 					break;
 				case "S":
-					arr= [3, 4, 5];
+					arr= [4, 5, 3, 4];
 					break;
 				case "Z":
-					arr= [3, 4, 5];
+					arr= [3, 4, 4, 5];
 					break;
 			}
-			return arr;
-		}
-		function getY(){
-			var arr= [];
-			switch(self.shape_name[self.ShapeList[self.cnt]]){
+		}else{
+			switch(this.ShapeList[this.cnt]){
 				case "I":
 					arr= [-1, -1, -1, -1];
 					break;
 				case "O":
-					arr= [0, 0];
+					arr= [-1, -1, 0, 0];
 					break;
 				case "T":
-					arr= [0, 0, 0];
+					arr= [-1, 0, 0, 0];
 					break;
 				case "L":
-					arr= [0, 0, 0];
+					arr= [-1, 0, 0, 0];
 					break;
 				case "J":
-					arr= [0, 0, 0];
+					arr= [-1, 0, 0, 0];
 					break;
 				case "S":
-					arr= [0, 0, -1];
+					arr= [-1, -1, 0, 0];
 					break;
 				case "Z":
-					arr= [-1, 0, 0];
+					arr= [-1, -1, 0, 0];
 					break;
 			}
-			return arr;
 		}
-		var self= this;
-
-		this.arr= [
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		];
+		return arr;
+	};
+	Tetris.prototype.next = function() {
+		var len= this.arr.length;
+		var minX= Math.min.apply(null, this.Nx);
+		var maxX= Math.max.apply(null, this.Nx);
+		var maxY= Math.max.apply(null, this.Ny);
+		if(maxY+1 >= 20)
+			return true;
+		for(var j=0; j<len; j++){
+			if(minX <= this.arr[j][0] && this.arr[j][0] <= maxX && this.arr[j][1] - maxY < 3){
+				for(var i=0; i<this.Nx.length; i++){
+					if(this.Nx[i] == this.arr[j][0] && this.Ny[i]+1 == this.arr[j][1])
+						return true;
+				}
+			}
+		}
+		return false;
+	};
+	Tetris.prototype.block = function(x, y, n) {
+		this.ctx.save();
+		this.ctx.fillStyle= n;
+		this.ctx.lineWidth= 3;
+		this.ctx.strokeStyle= "#000";
+		this.ctx.fillRect(x*this.Bw+3, y*this.Bh+20+3, this.Bw-6, this.Bh-6);
+		this.ctx.strokeRect(x*this.Bw+1.5, y*this.Bh+20+1.5, this.Bw-3, this.Bh-3);
+		this.ctx.restore();
+	};
+	Tetris.prototype.NextBlock = function() {
+		for(var i=0; i< this.Nx.length; i++){
+		this.arr.push([this.Nx[i], this.Ny[i], this.ShapeList[this.cnt]]);
+		}
+		if(this.arr.length > 10){
+			var a=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+			var len= this.arr.length;
+			var isFull= false
+			var FullNum= 20;
+			for(var i=0; i<len; i++)
+				a[this.arr[i][1]]++;
+			for(var i=0; i<20; i++){
+				if(a[i] == 10){
+					isFull= true;
+					FullNum= i < FullNum? i: FullNum;
+				}
+			}
+			if(isFull){
+				this.arr= this.arr.filter(function (v){
+					return a[v[1]] != 10 && v[0] < FullNum;
+				});
+				this.arr.map(function (v){
+					if(v[1] < FullNum)
+						return v[1]++;
+				})
+			}
+		}
+		this.cnt++;
+		if(this.cnt == this.ShapeList.length){
+			this.sort();
+			this.cnt=0;
+		}
+		this.Nx= this.getPos(true);
+		this.Ny= this.getPos(false);
+	};
+	Tetris.prototype.init = function (){
+		this.strokeStyle= "#333";
+		this.lineWidth= 1;
+		this.arr=[];
 		this.score= 0;
-		this.ShapeList=[];
-		this.sort();
 		this.cnt= 0;
-		this.Nx= getX();
-		this.Ny= getY();
-
-		self.display();
-		self.shape_draw[self.shape_name[self.ShapeList[self.cnt]]](self.Nx[0], self.Ny[0]);
-
-		function next(){
-			var a=0;
-			for(var i=0; i<self.Ny.length; i++){
-				if(self.arr[self.Ny[i]+1] != undefined && self.arr[self.Ny[i]+1][self.Nx[i]] != undefined && self.arr[self.Ny[i]+1][self.Nx[i]] == 0 )
-					a++;
-			}
-			return a!=self.Ny.length;
-		}
-		function addarr(){
-			switch(self.shape_name[self.ShapeList[self.cnt]]){
-				case "I":
-					for(var i=0; i< self.Ny.length; i++){
-						self.arr[self.Ny[i]][self.Nx[i]]=self.shape_name[self.ShapeList[self.cnt]];
-					}
-					break;
-				case "O":
-					for(var i=0; i< self.Ny.length; i++){
-						self.arr[self.Ny[i]][self.Nx[i]]=self.shape_name[self.ShapeList[self.cnt]];
-					}
-					for(var i=0; i< self.Ny.length; i++){
-						self.arr[self.Ny[i]-1][self.Nx[i]]=self.shape_name[self.ShapeList[self.cnt]];
-					}
-					break;
-				case "T":
-					for(var i=0; i< self.Ny.length; i++){
-						self.arr[self.Ny[i]][self.Nx[i]]=self.shape_name[self.ShapeList[self.cnt]];
-					}
-					self.arr[self.Ny[1]-1][self.Nx[1]]=self.shape_name[self.ShapeList[self.cnt]];
-					break;
-				case "L":
-					for(var i=0; i< self.Ny.length; i++){
-						self.arr[self.Ny[i]][self.Nx[i]]=self.shape_name[self.ShapeList[self.cnt]];
-					}
-					self.arr[self.Ny[0]-1][self.Nx[0]]=self.shape_name[self.ShapeList[self.cnt]];
-					break;
-				case "J":
-					for(var i=0; i< self.Ny.length; i++){
-						self.arr[self.Ny[i]][self.Nx[i]]=self.shape_name[self.ShapeList[self.cnt]];
-					}
-					self.arr[self.Ny[2]-1][self.Nx[2]]=self.shape_name[self.ShapeList[self.cnt]];
-					break;
-				case "S":
-					for(var i=0; i< self.Ny.length; i++){
-						self.arr[self.Ny[i]][self.Nx[i]]=self.shape_name[self.ShapeList[self.cnt]];
-					}
-					self.arr[self.Ny[1]-1][self.Nx[1]]=self.shape_name[self.ShapeList[self.cnt]];
-					break;
-				case "Z":
-					for(var i=0; i< self.Ny.length; i++){
-						self.arr[self.Ny[i]][self.Nx[i]]=self.shape_name[self.ShapeList[self.cnt]];
-					}
-					self.arr[self.Ny[1]-1][self.Nx[1]]=self.shape_name[self.ShapeList[self.cnt]];
-					break;
-			}
-			self.arr
-		}
-		function callback (){
-			addarr();
-			self.cnt++;
-			if(self.cnt == self.ShapeList.length){
-				self.sort();
-				self.cnt=0;
-			}
-			self.Nx= getX();
-			self.Ny= getY();
-			setTimeout(function (){
-				self.display();
-				self.shape_draw[self.shape_name[self.ShapeList[self.cnt]]](self.Nx[0], self.Ny[0]);
-				down= setInterval(_itv, 500); 
-			}, 500);
-		}
-		function _itv (){
-			for(var i=0; i<self.Ny.length; i++){
-				self.Ny[i]++;
-			}
-			self.display();
-			self.shape_draw[self.shape_name[self.ShapeList[self.cnt]]](self.Nx[0], self.Ny[0]);
-			if(next()){
-				clearInterval(down);
-				callback();
-			}
-		}
-		var down= setInterval(_itv, 500);
+		this.sort();
+		this.Nx= this.getPos(true);
+		this.Ny= this.getPos(false);	
+		this.eve();
 	};
 	Tetris.prototype.eve = function() {
 		var self= this;
 		window.addEventListener("keydown", function (e){
 			switch(e.keyCode){
+				// space, turn
+				case 32:
+					break;
+				case 38:
+					break;
+				// space, turn
 				case 37:
-					if(0<=self.x-1)
-						self.x--;
+					var Min= Math.min.apply(null, self.Nx);
+					if(Min==0)
+						return false;
+					for(var i=0; i<self.arr.length; i++){
+						if(self.arr[i][0] == Min-1){
+							for(var j=0; j<self.Nx.length; j++){
+								if(self.arr[i][0] == self.Nx[j]-1 && self.arr[i][1] == self.Ny[j]){
+									return false;
+								}
+							}
+						}
+					}
+					for(var i=0; i<self.Nx.length; i++)
+						self.Nx[i]--;
+					self.display();
 					break;
 				case 39:
-					if(self.x+1+self.len[0]<=10)
-						self.x++;
+					var Max= Math.max.apply(null, self.Nx);
+					if(Max==9)
+							return false;
+					for(var i=0; i<self.arr.length; i++){
+						if(self.arr[i][0] == Max+1){
+							for(var j=0; j<self.Nx.length; j++){
+								if(self.arr[i][0] == self.Nx[j]+1 && self.arr[i][1] == self.Ny[j]){
+									return false;
+								}
+							}
+						}
+					}
+					for(var i=0; i<self.Nx.length; i++)
+						self.Nx[i]++;
+					self.display();
 					break;
 				case 40:
-					if(self.y+1+self.len[1]<=20)
-						self.y++;
+					if(self.next()){
+						self.NextBlock();
+					}else{
+						for(var i=0; i<self.Ny.length; i++)
+							self.Ny[i]++;
+					}
+					self.display();
 					break;
 			}
 		});
 	};
-	Tetris.prototype.check = function() {
-		for(var i=0; i<this.Nx.length; i++){
-			if(this.arr[this.ny[i]+1] == undefined || this.arr[this.ny[i]+1] != 0) // reach floor
-				return false;
+	Tetris.prototype.update = function (){
+		if(this.next()){
+			this.NextBlock();
+		}else{
+			for(var i=0; i<this.Ny.length; i++)
+				this.Ny[i]++;
 		}
-		return true;
-	};
-	Tetris.prototype.update = function (n){
-			if(n!=0&&n%60==0){ // 1s down
-				if(this.check()){ // reach floor
-					this.next();
-				}else{ // keep down
-					this.down();
-				}
-				this.display();
-			}
 
 		var self= this;
 		setTimeout(function (){
-			self.update(++n);
-		}, 1000/60);
+			self.update();
+		}, 800);
 	};
 	Tetris.prototype.display = function (){
-		// 10 * 20
 		this.ctx.clearRect(0, 0, this.W, this.H);
-		this.strokeStyle= "#333";
-		this.lineWidth= 1;
 		this.ctx.beginPath();
 		for(var j=0; j<= 19; j++){
 			this.ctx.moveTo(0, j*this.Bh+20);
@@ -312,58 +232,67 @@
 			this.ctx.stroke();
 		}
 		this.ctx.closePath();
+		var c="";
 		for(var i=0; i< this.arr.length; i++){
-			for(var j=0; j< this.arr[i].length; j++){
-				switch(this.arr[i][j]){
-					case "I":
-						var c= "#364fc7";
-						this.block(j*this.Bw, i*this.Bh+20, c);
-						break;
-					case "O":
-						var c= "#fcc419";
-						this.block(j*this.Bw, i*this.Bh+20, c);
-						break;
-					case "T":
-						var c= "#845ef7";
-						this.block(j*this.Bw, i*this.Bh+20, c);
-						break;
-					case "L":
-						var c= "#e8590c";
-						this.block(j*this.Bw, i*this.Bh+20, c);
-						break;
-					case "J":
-						var c= "#74c0fc";
-						this.block(j*this.Bw, i*this.Bh+20, c);
-						break;
-					case "S":
-						var c= "#8ce99a";
-						this.block(j*this.Bw, i*this.Bh+20, c);
-						break;
-					case "Z":
-						var c= "#f03e3e";
-						this.block(j*this.Bw, i*this.Bh+20, c);
-						break;
-				}
+			switch(this.arr[i][2]){
+				case "I":
+					c= "#364fc7";
+					break;
+				case "O":
+					c= "#fcc419";
+					break;
+				case "T":
+					c= "#845ef7";
+					break;
+				case "L":
+					c= "#e8590c";
+					break;
+				case "J":
+					c= "#74c0fc";
+					break;
+				case "S":
+					c= "#8ce99a";
+					break;
+				case "Z":
+					c= "#f03e3e";
+					break;
 			}
+			this.block(this.arr[i][0], this.arr[i][1], c);
 		}
-
+		switch(this.ShapeList[this.cnt]){
+			case "I":
+				c= "#364fc7";
+				break;
+			case "O":
+				c= "#fcc419";
+				break;
+			case "T":
+				c= "#845ef7";
+				break;
+			case "L":
+				c= "#e8590c";
+				break;
+			case "J":
+				c= "#74c0fc";
+				break;
+			case "S":
+				c= "#8ce99a";
+				break;
+			case "Z":
+				c= "#f03e3e";
+				break;
+		}
+		this.block(this.Nx[0], this.Ny[0], c);
+		this.block(this.Nx[1], this.Ny[1], c);
+		this.block(this.Nx[2], this.Ny[2], c);
+		this.block(this.Nx[3], this.Ny[3], c);
 		var self= this;
-		// requestAnimationFrame(function(){self.display();});
-	};
-	Tetris.prototype.block = function(x, y, n) {
-		this.ctx.save();
-		this.ctx.fillStyle= n;
-		this.ctx.lineWidth= 3;
-		this.ctx.strokeStyle= "#000";
-		this.ctx.fillRect(x+3, y+3, this.Bw-6, this.Bh-6);
-		this.ctx.strokeRect(x+1.5, y+1.5, this.Bw-3, this.Bh-3);
-		this.ctx.restore();
+		requestAnimationFrame(function(){self.display();});
 	};
 	window.onload= function (){
 		var tetris= new Tetris();
 		tetris.init();
-		// tetris.update(0);
-		// tetris.display();
+		tetris.update();
+		tetris.display();
 	}
-	return Tetris;
 })()
